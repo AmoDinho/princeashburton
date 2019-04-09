aeff15d380fdeabc110f88f63d8687007900fb17
amodinho <u14284783@tuks.co.za>
2019-04-09 15:34:49 +0200

    removed stuff on index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,9 +2,8 @@
   <Layout>
     
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1>Hello, world!</h1>
+     
    
     <p>
       This is my portfolio page!
