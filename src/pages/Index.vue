b4f1d9e682444d64b53f267717ae0b775d632ff4
amodinho <u14284783@tuks.co.za>
2019-04-09 15:17:53 +0200

    Scaffolded site with Gridsome

---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>Hello, world!</h1>
+   
+    <p>
+      This is my portfolio page!
+    </p>
+
+   
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Prince Ashburton'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
