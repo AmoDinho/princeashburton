c801bce057f5a08a1331fc41017b76bc88142e34
amodinho <u14284783@tuks.co.za>
2019-04-18 16:32:14 +0200

    IDK

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,13 @@
     <div class="max-w-sm  rounded-lg card overflow-hidden shadow-1 ">
       <h1 class="font-display text-center">Amo Moloko</h1>
       <h3 class="font-display text-lg text-center text-grey">Product Designer/Full-Stack Engineer </h3>
-      <h3 class="font-display text-lg text-center "> CTO @ <a href="www.pimpmybook.co.za" class="no-underline hover:underline text-pink">Pimp My Book</a></h3>
+      <h3 class="font-display text-lg text-center ">
+         CTO @ 
+         <a href="www.pimpmybook.co.za"
+          class="no-underline hover:underline text-pink">
+          Pimp My Book
+          </a>
+          </h3>
       <div class="flex justify-center  card__canopy">
 
           <g-image 
