6268f5a95fc7beeab78fa8da12c6aeb0f26a053a
amodinho <u14284783@tuks.co.za>
2019-04-23 17:32:20 +0200

    Added 'vue-svg-loader'

--- gridsome.config.js
+++ gridsome.config.js
@@ -12,3 +12,13 @@ module.exports = {
     }
   ]
 }
+
+module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  }
+}
