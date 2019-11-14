ba0190ded220871ccb26f7b7568510f2dcbc8176
AmoDinho <u14284783@tuks.co.za>
2019-11-14 12:40:03 +0200

    added the props

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,13 +1,32 @@
-<template></template>
+<template>
+  <div>
+    <SubHeading>{{projectName}}</SubHeading>
+    <BodyText>{{description}}</BodyText>
+
+    <BodyText>Technogies: {{tech}}</BodyText>
+
+    <div>
+      <BodyText>Live url:{{url1}}</BodyText>
+      <BodyText>Git repo:{{gitRepo}}</BodyText>
+    </div>
+  </div>
+</template>
 <script>
+import SubHeading from "../SubHeading";
+import BodyText from "../BodyText";
 export default {
   name: "ProjectCard",
+  components: {
+    SubHeading,
+    BodyText
+  },
   props: {
     projectName: String,
     url1: String,
     gitRepo: String,
     url2: String,
-    description: String
+    description: String,
+    tech: Object
   }
 };
 </script>
