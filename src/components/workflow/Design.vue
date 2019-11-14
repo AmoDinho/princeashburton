0e8dedc934da4eee209304cac037c3c0ca0b5845
AmoDinho <u14284783@tuks.co.za>
2019-11-14 10:53:51 +0200

    done with the design part

--- src/components/workflow/Design.vue
+++ src/components/workflow/Design.vue
@@ -9,6 +9,14 @@
       <BodyText>My design work usually starts with asking the question of who,what and how. This leads to gathering user research, which I then synthesis into user stories/use cases and start working on UI/UX, which usually yields mockups/prototypes.</BodyText>
 
       <BodyText>Usually all this cannot be done without some logical project management and agile approaches.</BodyText>
+
+      <BodyText>Tools and techniques I use:</BodyText>
+      <ul>
+        <li>Figma</li>
+        <li>Airtable</li>
+        <li>Draw.io</li>
+        <li>Structual analysis which normally yields: Activity, state or Sequence diagrams</li>
+      </ul>
     </div>
   </div>
 </template>
