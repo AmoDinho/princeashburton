cedbf9268ea0afd4a2ce506407beebc07bb705a0
AmoDinho <u14284783@tuks.co.za>
2019-11-14 12:05:01 +0200

    created project index

---
+++ src/components/projects/ProjectsIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <SubHeading>Here are some projects I've made:</SubHeading>
+  </div>
+</template>
+
+<script>
+import SubHeading from "../SubHeading";
+import BodyText from "../BodyText";
+export default {
+  name: "ProjectsIndex",
+  components: {
+    SubHeading,
+    BodyText
+  }
+};
+</script>
