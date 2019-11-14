86c46916eaf5d5ad2d80f2fc06cb975e18c27fc7
AmoDinho <u14284783@tuks.co.za>
2019-11-14 10:48:14 +0200

    importing body text

--- src/components/workflow/Design.vue
+++ src/components/workflow/Design.vue
@@ -3,14 +3,20 @@
     <div>
       <SubHeading>A bit about how go about making things</SubHeading>
     </div>
+
+    <div>
+      <SubHeading>Product Design</SubHeading>
+    </div>
   </div>
 </template>
 <script>
 import SubHeading from "../SubHeading";
+import BodyText from "../BodyText";
 export default {
   name: "Design",
   components: {
-    SubHeading
+    SubHeading,
+    BodyText
   }
 };
 </script>
