89f62f83d6d3cb31ac316d0284123aae295bb9bb
AmoDinho <u14284783@tuks.co.za>
2019-11-14 10:39:47 +0200

    added bodytext component

---
+++ src/components/BodyText.vue
@@ -0,0 +1,15 @@
+<template>
+         <p class="font-body">
+         {{text}}
+         </p>
+</template>
+
+<script>
+export default {
+    name: 'BodyText',
+    props: {
+        text: String
+    }
+}
+</script>
+
