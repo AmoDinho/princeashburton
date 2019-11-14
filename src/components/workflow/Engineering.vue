3d342eaa80bf883ce2ea8e08ee1be3f929fd1265
AmoDinho <u14284783@tuks.co.za>
2019-11-14 11:03:57 +0200

    Added my skill set

--- src/components/workflow/Engineering.vue
+++ src/components/workflow/Engineering.vue
@@ -1,5 +1,28 @@
 <template>
-  <SubHeading>Fullstack Engineering</SubHeading>
+  <div>
+    <SubHeading>Fullstack Engineering</SubHeading>
+    <BodyText>
+      I enjoy architecting fullstack
+      applications on the cloud. I can
+      make either consumer apps
+      (web) or developer tools which I
+      use JavaScript. My Machine
+      Learning is usually done in
+      Python.
+    </BodyText>
+    <BodyText>My Skillset</BodyText>
+    <ul>
+      <li>React</li>
+      <li>Vue</li>
+      <li>Netlify</li>
+      <li>AWS</li>
+      <li>SciKitLearn</li>
+      <li>FastAI</li>
+      <li>CI/CD</li>
+      <li>GraphQL</li>
+      <li>SQL/No-SQL</li>
+    </ul>
+  </div>
 </template>
 
 <script>
