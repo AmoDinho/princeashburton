d438534792df48d25dd5a663c4631f7d5e933c56
AmoDinho <u14284783@tuks.co.za>
2019-11-14 10:49:59 +0200

    all the text is rendering

--- src/components/workflow/Design.vue
+++ src/components/workflow/Design.vue
@@ -6,6 +6,9 @@
 
     <div>
       <SubHeading>Product Design</SubHeading>
+      <BodyText>My design work usually starts with asking the question of who,what and how. This leads to gathering user research, which I then synthesis into user stories/use cases and start working on UI/UX, which usually yields mockups/prototypes.</BodyText>
+
+      <BodyText>Usually all this cannot be done without some logical project management and agile approaches.</BodyText>
     </div>
   </div>
 </template>
