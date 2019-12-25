0b8e928304dedd4d1254da5c4528d1a20ef7972a
AmoDinho <u14284783@tuks.co.za>
2019-12-25 14:29:49 +0200

    fixed magin issue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,39 +1,31 @@
 <template>
-    <div class="max-w-sm  rounded-lg card overflow-hidden shadow-1 ">
-      <h1 class="font-display text-center">Amo Moloko</h1>
-      <h3 class="font-display text-lg text-center text-grey">Product Designer/Full-Stack Engineer </h3>
-    
-      <div class="flex justify-center  card__canopy">
-
-          <g-image 
-          class="rounded-full h-32 w-32 absolute "
-           src="~/assets/me_2019.png" />
-      </div>
+  <div class="max-w-sm rounded-lg card overflow-hidden shadow-1">
+    <h1 class="font-display text-center">Amo Moloko</h1>
+    <h3 class="font-display text-lg text-center text-grey">Product Designer/Full-Stack Engineer</h3>
+    <h3 class="mt-8 text-white"></h3>
+    <div class="flex justify-center card__canopy">
+      <g-image class="rounded-full h-32 w-32 absolute" src="~/assets/me_2019.png" />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Card'
-}
+  name: "Card"
+};
 </script>
 
 <style >
 .card {
-   
-   width: 400px;
-    height: 200px;
-    background-color: #ffffff;
+  width: 400px;
+  height: 200px;
+  background-color: #ffffff;
 }
 
 .card__canopy {
   z-index: 8;
   top: 152px;
-  
- }
-
-
-
+}
 </style>
 
 
