ec910ffaf92d422b252449d438e460aa756d82d7
Amo Moloko <u14284783@tuks.co.za>
2020-01-10 14:49:14 +0200

    Update Design.vue

--- src/components/workflow/Design.vue
+++ src/components/workflow/Design.vue
@@ -6,7 +6,7 @@
 
     <div>
       <SubHeading class="mt-5">Product Design</SubHeading>
-      <BodyText>My design work usually starts with asking the question of who,what and how. This leads to gathering user research, which I then synthesis into user stories/use cases and start working on UI/UX, which usually yields mockups/prototypes. I am also a big believer in design systems for projects that call for them.</BodyText>
+      <BodyText>My design work  starts with asking the question of who,what and how. This leads to gathering user research, which I then synthesis into user stories/use cases and start working on UI/UX, which yields mockups/prototypes. I am also a big believer in design systems for projects that call for them.</BodyText>
 
       <BodyText
         class="mt-3"
@@ -28,7 +28,7 @@
         </li>
         <li class="flex">
           <g-image src="~/assets/floppy-disk_1f4be.svg" class="mr-3 ss:mb-10" />
-          <BodyText>Structual analysis which normally yields: Activity, State or Sequence diagrams</BodyText>
+          <BodyText>Structual analysis which yields: Activity, State or Sequence diagrams</BodyText>
         </li>
       </ul>
     </div>
