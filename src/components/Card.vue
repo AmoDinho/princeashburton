a97b1bbb852e9209c539644c07030811dbb6c9da
amo.moloko <amogelang@tripplo.co>
2020-01-20 20:54:05 +0200

    added new picture

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,7 +4,7 @@
     <h3 class="font-display text-lg text-center text-grey">Product Designer/Full-Stack Engineer</h3>
     <h3 class="mt-2 mb-2 text-blue text-center">Front-End Engineer @ <a class="no-underline text-blue" href="https://tripplo.co">Tripplo</a></h3>
     <div class="flex justify-center card__canopy">
-      <g-image class="rounded-full h-32 w-32 absolute" src="~/assets/me_2019.png" />
+      <g-image class="rounded-full h-32 w-32 absolute" src="~/assets/me_2020.png" />
     </div>
   </div>
 </template>
