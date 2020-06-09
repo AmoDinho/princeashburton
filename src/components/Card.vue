be9e0b894baac7cd575a3a955933c8f96dbfb768
Amo Moloko <u14284783@tuks.co.za>
2020-06-09 12:40:08 +0200

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,7 @@
   <div class="max-w-sm rounded-lg card overflow-hidden shadow-1">
     <h1 class="font-display text-center">Amo Moloko</h1>
     <h3 class="font-display text-lg text-center text-grey">Product Designer/Full-Stack Engineer</h3>
-    <h3 class="mt-2 mb-2 text-blue text-center">Front-End Engineer @ <a class="no-underline text-blue" href="https://tripplo.co">Tripplo</a></h3>
+    <h3 class="mt-2 mb-2 text-blue text-center">Fullstack Engineer @ <a class="no-underline text-blue" href="https://tripplo.co">Tripplo</a></h3>
     <div class="flex justify-center card__canopy">
       <g-image class="rounded-full h-32 w-32 absolute" src="~/assets/me_2020.png" />
     </div>
