863f01e851f6da7c57173d41349abbe723637375
AmoDinho <u14284783@tuks.co.za>
2023-05-29 16:32:24 +0200

    added new copy

--- src/components/workflow/Engineering.vue
+++ src/components/workflow/Engineering.vue
@@ -2,12 +2,23 @@
   <div>
     <SubHeading class="mt-5">Fullstack Engineering</SubHeading>
     <BodyText>
-      I enjoy architecting fullstack
-      applications on the cloud. I can
-      make either consumer apps
-      (web) or developer tools in JavaScript. My Machine
-      Learning is done in
-      Python.
+      I am an analytical JavaScript engineer based in Cape Town, South Africa
+      who has a strong bias towards action. I primarily develop Fullstack Event
+      Driven applications primarily on AWS.
+    </BodyText>
+
+    <BodyText>
+      I’m a big believer in creating customer driven experiences, that have led
+      me to work in Retail and Logistics. These industries come with their fair
+      share of unique customer challenges which have resulted in learning when
+      to use particular technologies that best fit the needs of the problem at
+      hand.
+    </BodyText>
+
+    <BodyText>
+      When I’m not making technology, you can find me out on the ocean sailing,
+      hiking on a mountain or out and about trying to soak up nature with
+      friends & family.
     </BodyText>
     <BodyText class="mt-5 font-bold">My Skillset:</BodyText>
     <ul>
@@ -52,13 +63,13 @@
 </template>
 
 <script>
-import SubHeading from "../SubHeading";
-import BodyText from "../BodyText";
+import SubHeading from '../SubHeading';
+import BodyText from '../BodyText';
 export default {
-  name: "Engineering",
+  name: 'Engineering',
   components: {
     SubHeading,
-    BodyText
-  }
+    BodyText,
+  },
 };
 </script>
